9116b81b7957424e419d64ccf6f1ea8cd9d6f03a
mjulestek <mjulestek@users.noreply.github.com>
2026-02-27 13:54:13 +0100

    Update ALB DNS for new AWS account deployment

--- ubuntu-frontend/assets/js/config.js
+++ ubuntu-frontend/assets/js/config.js
@@ -1,3 +1,3 @@
 "use strict";
 // Backend API URL - points to ALB (Load Balancer)
-window.API_BASE = window.API_BASE || "http://ubuntu-travels-ALB-290703255.us-west-2.elb.amazonaws.com";
+window.API_BASE = window.API_BASE || "http://ubuntu-ALB-1803645338.us-west-2.elb.amazonaws.com";
